10d9cea2e7b58d86207288c0388f3bf0978a95a8
Jaime Dias <jaimejosediasnt@gmail.com>
2026-05-21 11:49:22 -0300

    rename: drop legacy riegel headers

--- include/riegel/riegel_mmio.h
+++
@@ -1,9 +0,0 @@
-#ifndef RIEGEL_MMIO_H
-#define RIEGEL_MMIO_H
-
-#include "riegel_types.h"
-
-riegel_u16 riegel_custom_read16(RiegelContext *ctx, riegel_u32 addr);
-void riegel_custom_write16(RiegelContext *ctx, riegel_u32 addr, riegel_u16 value);
-
-#endif
